c095ba062ba780be4019cea3c4cf0bb965594b96
Prasiddha Thapaliya <th.prasiddha@gmail.com>
2024-01-15 19:52:41 -0500

    Created a random request function to test testpath and testverb with original server paths and server verbs in an interval of 1000 milliseconds continusoly.

--- assignment1.js
+++ assignment1.js
@@ -12,6 +12,13 @@ let serverVerbs = ["GET", "GET", "GET", "POST", "GET", "POST"];
 let serverPaths = ["/", "/about", "/contact", "/login", "/panel", "/logout"];
 let serverResponses = ["Welcome to WEB700 Assignment 1", "This assignment was prepared by Prasiddha Thapaliya", "Prasiddha Thapaliya: pthapaliya@myseneca.ca", "User Logged In", "Main Panel", "Logout Complete"];
 
+let testVerbs = ["GET", "POST"];
+let testPaths = ["/", "/about", "/contact", "/login", "/panel", "/logout", "/randomPath1", "/randomPath2"];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
 function httpRequest(httpVerb, path){
     for(let i=0; i<serverPaths.length; i++){
         if(serverVerbs[i]=== httpVerb && serverPaths[i]=== path){
@@ -22,10 +29,24 @@ function httpRequest(httpVerb, path){
 }
 
 //manually testing "httpRequest" Function
-console.log(httpRequest("GET", "/")); 
-console.log(httpRequest("GET", "/about")); 
-console.log(httpRequest("GET", "/contact"));
-console.log(httpRequest("POST", "/login")); 
-console.log(httpRequest("GET", "/panel"));
-console.log(httpRequest("POST", "/logout")); 
+// console.log(httpRequest("GET", "/")); 
+// console.log(httpRequest("GET", "/about")); 
+// console.log(httpRequest("GET", "/contact"));
+// console.log(httpRequest("POST", "/login")); 
+// console.log(httpRequest("GET", "/panel"));
+// console.log(httpRequest("POST", "/logout")); 
+// console.log(httpRequest("POST", "/logot")); 
 
+
+function randomRequest() {
+    let randVerb = getRandomInt(testVerbs.length);
+    let randPath = getRandomInt(testPaths.length);
+    let result = httpRequest(testVerbs[randVerb], testPaths[randPath]);
+    console.log(result);
+}
+
+function automateTests(){
+    setInterval(randomRequest, 1000);
+}
+
+automateTests();
