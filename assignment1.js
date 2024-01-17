a8f20348bb988ed448c5b9bbbc411949783fbdf0
Prasiddha Thapaliya <th.prasiddha@gmail.com>
2024-01-16 20:12:33 -0500

    Updated In-line comments

--- assignment1.js
+++ assignment1.js
@@ -4,21 +4,21 @@
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Prasiddha Thapaliya Student ID:121569230  Date:14th Jan, 2023
+*  Name: Prasiddha Thapaliya Student ID:121569230  Date:16th Jan, 2023
 *
 ********************************************************************************/ 
-
+//arrays for specific verbs and path with respective reponses
 let serverVerbs = ["GET", "GET", "GET", "POST", "GET", "POST"];
 let serverPaths = ["/", "/about", "/contact", "/login", "/panel", "/logout"];
 let serverResponses = ["Welcome to WEB700 Assignment 1", "This assignment was prepared by Prasiddha Thapaliya", "Prasiddha Thapaliya: pthapaliya@myseneca.ca", "User Logged In", "Main Panel", "Logout Complete"];
-
+//arrays with testverbs and testpaths to randomly test the function
 let testVerbs = ["GET", "POST"];
 let testPaths = ["/", "/about", "/contact", "/login", "/panel", "/logout", "/randomPath1", "/randomPath2"];
-
+//function to get random values
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
-
+//function with for if loop to return httprequest responses as valid or not
 function httpRequest(httpVerb, path){
     for(let i=0; i<serverPaths.length; i++){
         if(serverVerbs[i]=== httpVerb && serverPaths[i]=== path){
@@ -27,8 +27,7 @@ function httpRequest(httpVerb, path){
     }
     return '400: Unable to process' + httpVerb + ' request for ' + path;
 }
-
-//manually testing "httpRequest" Function
+//testing "httpRequest" Function individually
 // console.log(httpRequest("GET", "/")); 
 // console.log(httpRequest("GET", "/about")); 
 // console.log(httpRequest("GET", "/contact"));
@@ -37,16 +36,16 @@ function httpRequest(httpVerb, path){
 // console.log(httpRequest("POST", "/logout")); 
 // console.log(httpRequest("POST", "/logot")); 
 
-
+//function to use randomverbs and randompaths with httpRequest function
 function randomRequest() {
     let randVerb = getRandomInt(testVerbs.length);
     let randPath = getRandomInt(testPaths.length);
     let result = httpRequest(testVerbs[randVerb], testPaths[randPath]);
     console.log(result);
 }
-
+//function to test with data in 1 second interval
 function automateTests(){
     setInterval(randomRequest, 1000);
 }
-
+//invoking the final function which has no loop end
 automateTests();
